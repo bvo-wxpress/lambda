1fafb78da32d018c6081f2f40c3504b736ba0b0c
Man "Bee" Vo <vo.bee92@gmail.com>
2018-03-24 10:29:53 -0500

    Format Code.

--- index.js
+++ index.js
@@ -2,76 +2,81 @@ exports.handler = (event, context, callback) => {
     var wxp_dynamo_data = null;
     var wxp_ec2_data = null;
     var ec2_public_dns = '';
+    
     //get the user name from api gateway
     var cognito_username = event.requestContext.authorizer.claims['cognito:username'];
     var email = event.requestContext.authorizer.claims['email'];
+    
     //FOR TESTING PURPOSES, REMOVE IN PRODUCTION
     // var cognito_username = 'ede97585-21c7-46f1-a5e2-8cc460bfca99';
     // var email = "vo.bee92@gmail.com";
-    
+
     // Load the AWS SDK for Node.js
     var AWS = require('aws-sdk');
+    
     // Set the region (May not be necessary)
     AWS.config.update({region: 'us-east-1'});
-    
+
     // Create the DynamoDB service object
     var dynamo = new AWS.DynamoDB(); //TODO SHOULD DEFINE API VERSION!
-    
+
     //Create EC2 Service object
     var ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
-    
+
     //create return object and response for debugging purposes.
     var obj = {
-    'cognito_username': cognito_username,
-    'email': email,
-        // 'event': event
+        'cognito_username': cognito_username,
+        'email': email,
     };
     var response = {
         "statusCode": 200,
-        "headers": { 
-            "Access-Control-Allow-Origin": "*" 
+        "headers": {
+            "Access-Control-Allow-Origin": "*"
         },
         "body": obj
     };
     
+    function init(){
+        getDynamoData();
+    }
 
     // Call DynamoDB to read the item from the table
     function getDynamoData(){
-       var params = {
+        var params = {
           TableName: 'WxPress',
           Key: {
             'UserId' : {S: cognito_username},
           }
         };
         dynamo.getItem(params, function(err, data) {
-          if (err) {
-            response.body.error = err;
-            send();
-          } else {
-            wxp_dynamo_data = data.Item;
-            response.body.wxpress_data = wxp_dynamo_data;
-            getEc2Data();
-          }
+            if (err) {
+                response.body.error = err;
+                send();
+            } else {
+                wxp_dynamo_data = data.Item;
+                response.body.wxpress_data = wxp_dynamo_data;
+                getEc2Data();
+            }
         });
     }
-    
+
     // fetch data of ec2 instance
     function getEc2Data(){
         var ec2Params = {
             InstanceIds: [wxp_dynamo_data.instanceId.S]
         };
         ec2.describeInstances(ec2Params, function(err, data) {
-          if (err) {
-            response.body.err = err;
-            send();
-          }else{
-            wxp_ec2_data = data.Reservations[0].Instances[0];
-            response.body.ec2 = wxp_ec2_data;
-            handleInstance();
-          }
+            if (err) {
+                response.body.err = err;
+                send();
+            } else {
+                wxp_ec2_data = data.Reservations[0].Instances[0];
+                response.body.ec2 = wxp_ec2_data;
+                handleInstance();
+            }
         });
     }
-    
+
     //Handler for instance state codes
     function handleInstance(){
         var instanceCode = wxp_ec2_data.State.Code;
@@ -91,7 +96,7 @@ exports.handler = (event, context, callback) => {
                 break;
         }
     }
-    
+
     //Initiate start of the ec2 instance
     function startEc2(){
         var params = {
@@ -107,26 +112,28 @@ exports.handler = (event, context, callback) => {
             }
         });
     }
-    
+
     //Wait for the ec2 instance to start
     function waitForEc2(){
         var params = {
-          'InstanceIds': [wxp_ec2_data.InstanceId]
+            'InstanceIds': [wxp_ec2_data.InstanceId]
         };
         ec2.waitFor('instanceRunning', params, function(err, data) {
-            if (err){
-              response.body.err = err;
-              send();
-            }else{
-              getEc2Data();
+            if (err) {
+                response.body.err = err;
+                send();
+            } else {
+                getEc2Data();
             }
         });
     }
-    
+
     //send response to endpoint
     function send(){
         response.body = JSON.stringify(response.body);
         callback(null, response);
     }
-    getDynamoData();
+    
+    //start!
+    init();
 };
